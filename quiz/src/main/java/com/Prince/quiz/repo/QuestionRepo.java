90629a3ea0a3f54453033a37cf5e9f20bad06f03
Prince8303 <150062540+Prince8303@users.noreply.github.com>
2025-08-22 12:45:07 +0530

    feat: implement Question entity, repo, and service

---
+++ quiz/src/main/java/com/Prince/quiz/repo/QuestionRepo.java
@@ -0,0 +1,10 @@
+package com.Prince.quiz.repo;
+
+
+import com.Prince.quiz.entity.QuizQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepo extends JpaRepository<QuizQuestion,Long> {
+}
