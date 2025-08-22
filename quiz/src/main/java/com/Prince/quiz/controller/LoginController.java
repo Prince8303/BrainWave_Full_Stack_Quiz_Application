25abeb0cdc59d024c60da723d054b0508accf3c4
Prince8303 <150062540+Prince8303@users.noreply.github.com>
2025-08-22 13:12:37 +0530

    updating LoginController

--- quiz/src/main/java/com/Prince/quiz/controller/LoginController.java
+++ quiz/src/main/java/com/Prince/quiz/controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.Prince.quiz.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.Prince.quiz.dto.LoginRequest;
+import com.Prince.quiz.entity.QuizQuestion;
+import com.Prince.quiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @CrossOrigin(origins = "http://localhost:8080")
@@ -11,5 +14,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class LoginController {
 
+    @Autowired
+    QuestionService questionService;
 
+    // Hardcoded credentials for now
+    private final String USERNAME="user";
+    private final String PASSWORD="password";
+
+
+    public String login(@RequestBody LoginRequest loginRequest)
+    {
+        if(USERNAME.equals(loginRequest.getUsername()) && PASSWORD.equals(loginRequest.getPassword()))
+        {
+            return "Login Successful";
+        }
+        else
+        {
+            return "Invalid Username or Password";
+        }
+    }
+
+    @GetMapping("/questions")
+    public List<QuizQuestion> getQuestions()
+    {
+        return questionService.getAllQuestions();
+    }
+
+    @PostMapping(value = "/save", consumes = "application/json", produces = "application/json")
+    public QuizQuestion saveQuestion(@RequestBody QuizQuestion question)
+    {
+        return questionService.saveQuestion(question);
+    }
 }
